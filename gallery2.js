8fcfb47a8f023144ca9a3fbd636489528fe73f00
Kashinka <azalena@yandex.ru>
2023-03-30 17:09:41 +0300

    corrected names of vars

--- gallery2.js
+++ gallery2.js
@@ -1,16 +1,16 @@
-var i=0;
+var i1=0;
         var image = document.getElementById("image1");
         // Добавьте свои картинки через запятую
-        var imgs = new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
+        var imgs1 = new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
         function imgsrc1() {
             i++;
             image.src=imgs[i];
         }
 
-var i=0;
+var i2=0;
         var image = document.getElementById("image2");
         // Добавьте свои картинки через запятую
-        var imgs = new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
+        var imgs2 = new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
         function imgsrc2() {
             i++;
             image.src=imgs[i];
