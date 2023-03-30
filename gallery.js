625320d37ecf5ef825d75a9a7507e19d84fdb855
Kashinka <azalena@yandex.ru>
2023-03-30 12:39:52 +0300

    Added functions

---
+++ gallery.js
@@ -0,0 +1,17 @@
+var 1i=0;
+        var image=document.getElementById("image");
+        // Добавьте свои картинки через запятую
+        var imgs=new Array( '1.jpg','2.jpg' );
+        function imgsrc() {
+            i++;
+            image.src=imgs[i];
+        }
+
+var 1i=0;
+        var image=document.getElementById("image2");
+        // ДобавЬте свои картинки через запятую
+        var imgs=new Array( '1.jpg','2.jpg' );
+        function imgsrc2() {
+            i++;
+            image.src=imgs[i];
+        }
