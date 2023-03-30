4865013b1a81ed2b27a8f1bc875ceaba5770e5a4
Kashinka <azalena@yandex.ru>
2023-03-30 16:41:32 +0300

    Corrected links

--- gallery2.js
+++ gallery2.js
@@ -1,7 +1,8 @@
 var i=0;
         var image=document.getElementById("image");
         // Добавьте свои картинки через запятую
-        var imgs=new Array('note.png','note_30.png', 'note_30_.png');
+        var imgs=new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
         function imgsrc() {
             i++;
             image.src=imgs[i];
+        }
