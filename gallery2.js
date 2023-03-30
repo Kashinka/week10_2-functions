f26b0b0bf3c90dc85a7e2392955e8a5615d01bc4
Kashinka <azalena@yandex.ru>
2023-03-30 16:45:23 +0300

    Вщгидув агтсешщт

--- gallery2.js
+++ gallery2.js
@@ -2,7 +2,16 @@ var i=0;
         var image=document.getElementById("image");
         // Добавьте свои картинки через запятую
         var imgs=new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
-        function imgsrc() {
+        function imgsrc1() {
+            i++;
+            image.src=imgs[i];
+        }
+
+var i=0;
+        var image=document.getElementById("image");
+        // Добавьте свои картинки через запятую
+        var imgs=new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
+        function imgsrc2() {
             i++;
             image.src=imgs[i];
         }
