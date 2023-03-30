298774cd15ad389b183618c07019581343586100
Kashinka <azalena@yandex.ru>
2023-03-30 17:14:57 +0300

    Corrected names of vars

--- gallery2.js
+++ gallery2.js
@@ -1,5 +1,5 @@
 var i1=0;
-        var image = document.getElementById("image1");
+        var image1 = document.getElementById("image1");
         // Добавьте свои картинки через запятую
         var imgs1 = new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
         function imgsrc1() {
@@ -8,7 +8,7 @@ var i1=0;
         }
 
 var i2=0;
-        var image = document.getElementById("image2");
+        var image2 = document.getElementById("image2");
         // Добавьте свои картинки через запятую
         var imgs2 = new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
         function imgsrc2() {
