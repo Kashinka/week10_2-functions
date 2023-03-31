3b32676ea94c15f4eacf925142c9827f7ef1ff30
Kashinka <azalena@yandex.ru>
2023-03-31 23:06:10 +0300

    Changed imgsrc2

--- gallery2.js
+++ gallery2.js
@@ -9,6 +9,16 @@ function imgsrc1() {
 }
 
 function imgsrc2() {
-        i--;
+    if (i > 2) {
+        i = 0;
         image.src=imgs[i];
+    }
+    if (i < 1) {
+        image.src=imgs[1];
+    }
+    else {
+        i--;
+        image.src=imgs.src[i];
+    }
+    
 }
