2e26cf4e5ebd60c54b98263597133abfc3def50a
Kashinka <azalena@yandex.ru>
2023-03-31 21:19:48 +0300

    Added spaces

--- gallery2.js
+++ gallery2.js
@@ -1,7 +1,7 @@
 var i1=0;
         var image1 = document.getElementById("image1");
         // Добавьте свои картинки через запятую
-        var imgs1 = new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
+        var imgs1 = new Array( 'assets/img/1.jpg', 'assets/img/2.jpg' );
         function imgsrc1() {
             i1++;
             image1.src=imgs1[i1];
@@ -10,7 +10,7 @@ var i1=0;
 var i2=0;
         var image2 = document.getElementById("image2");
         // Добавьте свои картинки через запятую
-        var imgs2 = new Array( 'assets/img/1.jpg','assets/img/2.jpg' );
+        var imgs2 = new Array( 'assets/img/1.jpg', 'assets/img/2.jpg' );
         function imgsrc2() {
             i2++;
             image2.src=imgs2[i2];
