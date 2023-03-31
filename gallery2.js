67ebbeaac9b05ea859744563252a58efa6474ed0
Kashinka <azalena@yandex.ru>
2023-03-31 22:41:29 +0300

    Changed var image

--- gallery2.js
+++ gallery2.js
@@ -1,16 +1,14 @@
 var i = 0;
-var image1 = document.getElementById("image1");
+var image = document.getElementById("image");
         // Добавьте свои картинки через запятую
 var imgs = new Array( 'assets/img/1.jpg', 'assets/img/2.jpg' );
 
 function imgsrc1() {
-        image1.src=imgs[i];
+        image.src=imgs[i];
         i++;
 }
 
-var image2 = document.getElementById("image2");
-
 function imgsrc2() {
         i--;
-        image2.src=imgs[i];
+        image.src=imgs[i];
 }
