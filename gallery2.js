d024fb4567fabd6c7cd6e7eb20d23face089df7c
Kashinka <azalena@yandex.ru>
2023-03-31 21:28:15 +0300

    Changed functions and var imgs

--- gallery2.js
+++ gallery2.js
@@ -1,19 +1,16 @@
-var i1 = 0;
+var i = 0;
 var image1 = document.getElementById("image1");
         // Добавьте свои картинки через запятую
-var imgs1 = new Array( 'assets/img/1.jpg', 'assets/img/2.jpg' );
+var imgs = new Array( 'assets/img/1.jpg', 'assets/img/2.jpg' );
 
 function imgsrc1() {
-        i1++;
-        image1.src=imgs1[i1];
+        image1.src=imgs[i];
+        i++;
 }
 
-var i2 = 0;
 var image2 = document.getElementById("image2");
-        // Добавьте свои картинки через запятую
-var imgs2 = new Array( 'assets/img/1.jpg', 'assets/img/2.jpg' );
 
 function imgsrc2() {
-        i2++;
-        image2.src=imgs2[i2];
+        i--;
+        image2.src=imgs[i];
 }
