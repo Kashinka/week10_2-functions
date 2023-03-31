960ca204eda7201e514ac9929d8ad19ad1b3e894
Kashinka <azalena@yandex.ru>
2023-03-31 21:21:57 +0300

    Corrected spaces

--- gallery2.js
+++ gallery2.js
@@ -1,17 +1,19 @@
 var i1 = 0;
-        var image1 = document.getElementById("image1");
+var image1 = document.getElementById("image1");
         // Добавьте свои картинки через запятую
-        var imgs1 = new Array( 'assets/img/1.jpg', 'assets/img/2.jpg' );
-        function imgsrc1() {
-            i1++;
-            image1.src=imgs1[i1];
-        }
+var imgs1 = new Array( 'assets/img/1.jpg', 'assets/img/2.jpg' );
+
+function imgsrc1() {
+        i1++;
+        image1.src=imgs1[i1];
+}
 
 var i2 = 0;
-        var image2 = document.getElementById("image2");
+var image2 = document.getElementById("image2");
         // Добавьте свои картинки через запятую
-        var imgs2 = new Array( 'assets/img/1.jpg', 'assets/img/2.jpg' );
-        function imgsrc2() {
-            i2++;
-            image2.src=imgs2[i2];
-        }
+var imgs2 = new Array( 'assets/img/1.jpg', 'assets/img/2.jpg' );
+
+function imgsrc2() {
+        i2++;
+        image2.src=imgs2[i2];
+}
